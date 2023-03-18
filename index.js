5098edaa0331623f2362fe41e4d76d03bcc34551
Md Shohag <mdshohag119764@gmail.com>
2023-03-18 22:57:36 +0600

    Db connected and specialty get

--- index.js
+++ index.js
@@ -8,6 +8,47 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@daktarbari.abvhg0c.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect((err) => {
+//   // const collection = client.db("test").collection("devices");
+//   const specialtyCollection = client.db("Daktar-bari").collection("specialty");
+
+//   app.get("/specialty", async (req, res) => {
+//     const query = {};
+//     const cursor = specialtyCollection.find(query);
+//     const specialty = await cursor.toArray();
+//     res.send(specialty);
+//   });
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("db connected");
+
+    const specialtyCollection = client
+      .db("Daktar-bari")
+      .collection("specialty");
+    app.get("/specialty", async (req, res) => {
+      const query = {};
+      const cursor = specialtyCollection.find(query);
+      const specialty = await cursor.toArray();
+      res.send(specialty);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello from daktar bari server");
 });
